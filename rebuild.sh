50379b889572be2ded0986a2e5fb6d910e7af785
Lisandro Lorea <lisandrolorea@gmail.com>
2014-12-06 21:51:15 -0300

    Update rebuild.sh
    set autocrlf to whatever it was before running the script

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# I like core.autocrlf set to false, but it'll casue BuildTools to fail. 
-# If you have it set to true you can remove all the 'git config' lines here
+# Save status of core.autocrlf
+git_autocrlf=`git config --global core.autocrlf`
+# Set core.autocrlf to true (required by BuildTools)
 git config --global core.autocrlf true
 # Create a build directory and cd into it
 mkdir build
@@ -8,5 +9,5 @@ cd build
 # Download the latest version of BuildTools.jar and execute it
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar && 
 JAVA_HOME="C:\Program Files\Java\jdk1.8.0_25" java -jar BuildTools.jar ||
-(echo "Build failed" ; git config --global core.autocrlf false; exit 1)
-git config --global core.autocrlf false
+(echo "Build failed" ; git config --global core.autocrlf $git_autocrlf; exit 1)
+git config --global core.autocrlf $git_autocrlf
