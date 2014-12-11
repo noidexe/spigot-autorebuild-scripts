6c26a2449d278f1ca3a2030d96e5a2094645c14a
Lisandro Lorea <lisandrolorea@gmail.com>
2014-12-11 19:18:25 -0300

    Update rebuild.sh
    Info for linux users

--- rebuild.sh
+++ rebuild.sh
@@ -2,13 +2,13 @@
 # Save status of core.autocrlf
 git_autocrlf=`git config --global core.autocrlf`
 # Set core.autocrlf to true (required by BuildTools).
-git config --global core.autocrlf true #Comment out this line if on linux
+git config --global core.autocrlf true #NOTE: Comment out this line if on linux
 # Create a build directory and cd into it
 mkdir build
 cd build
 # Download the latest version of BuildTools.jar and execute it
-# Remove JAVA_HOME="C:\Program Files\Java\jdk1.8.0_25" if on
-# linux
+# NOTE: Remove JAVA_HOME="C:\Program Files\Java\jdk1.8.0_25" 
+# if on linux
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar && 
 JAVA_HOME="C:\Program Files\Java\jdk1.8.0_25" java -jar BuildTools.jar ||
 (echo "Build failed" ; git config --global core.autocrlf $git_autocrlf; exit 1)
