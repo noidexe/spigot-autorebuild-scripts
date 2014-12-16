dc20cf4cc36b6dd9ec9f2a33b395be3c5c9aab24
Lisandro Lorea <lisandrolorea@gmail.com>
2014-12-16 13:25:48 -0300

    Removed BuildData and Bukkit from the checked repos.
    No longer checks for changes in BuildData or in Bukkit when deciding if a rebuild is needed.

--- update-check.sh
+++ update-check.sh
@@ -5,11 +5,9 @@ mkdir build
 cd build
 # Remove old update-status files
 rm ../update-status
-cd BuildData; git fetch; git status remote >> ../../update-status; cd ..
-cd Bukkit; git fetch; git status remote >> ../../update-status; cd ..
 cd CraftBukkit; git fetch; git status remote >> ../../update-status; cd ..
 cd Spigot; git fetch; git status remote >> ../../update-status; cd ..
 # Count how many repositories are up to date
 UPTODATE=`grep -c up-to-date ../update-status`
 # If any of the repositories is outdated return exit code 1
-echo $UPTODATE | grep -q 4 || (echo "Only $UPTODATE repositories are up-to-date"; exit 1)
+echo $UPTODATE | grep -q 2 || (echo "Only $UPTODATE repositories are up-to-date"; exit 1)
